71938a2732e3ebaa344251118db3297b36f7c55a
MalikUzun <uzunmalik@hotmail.com>
2019-05-15 23:06:57 +0000

    ruby-v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  resources :oyuns
+  namespace :on_taraf do
+    get 'oyundetay/index'
+    get 'anasayfa/index'
+  end
+  
   resources :faydalars
   get 'panel', to: "admin#index"
-  namespace :on_taraf do
-   
-  end
   
   
   devise_for :users
-  root "on_taraf/ana_sayfa#index" #ana sayfa
+  
+  root "on_taraf/anasayfa#index" #ana sayfa
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_210423) do
+ActiveRecord::Schema.define(version: 2019_05_15_222855) do
 
-  create_table "faydalars", force: :cascade do |t|
-    t.string "baslik"
+  create_table "oyuns", force: :cascade do |t|
+    t.string "isim"
     t.text "aciklama"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "resim_file_name"
-    t.string "resim_content_type"
-    t.bigint "resim_file_size"
-    t.datetime "resim_updated_at"
-  end
-
-  create_table "uruns", force: :cascade do |t|
-    t.string "baslik"
-    t.text "aciklama"
-    t.integer "fiyat"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "resim_file_name"
